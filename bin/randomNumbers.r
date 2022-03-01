11aea495bb6f21dd1dcb3abe1a4261dc0e864199
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2022-03-01 10:48:31 -0500

    add randomNumers.r

--- bin/randomNumbers.r
+++ bin/randomNumbers.r
@@ -2,7 +2,7 @@
 numbers <- rep(-100, 15)
 numbers
 for (i in 1:15) { 
-    r = floor(runif(1, 1, 500))
+    r = floor(runif(1, 1, 500)) # one random number chozen from 1 to 500 
     if(length(numbers[abs(numbers - r)>20])){
         numbers[i] = r
     }
