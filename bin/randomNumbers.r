20e83ad7c8689f185e6c9772d546f4e082ddb56a
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2022-03-01 17:42:19 -0500

    add randomNumers.r

--- bin/randomNumbers.r
+++ bin/randomNumbers.r
@@ -1,10 +1,12 @@
 #!/usr/bin/env Rscript
 numbers <- rep(-100, 15)
 numbers
+
 for (i in 1:15) { 
     r = floor(runif(1, 1, 500)) # one random number chozen from 1 to 500 
-    if(length(numbers[abs(numbers - r)>20])){
-        numbers[i] = r
-    }
+    while(length(numbers[abs(numbers - r)>20]) != 15){
+        r = floor(runif(1, 1, 500))
+    } 
+    numbers[i] = r
 }
 numbers
