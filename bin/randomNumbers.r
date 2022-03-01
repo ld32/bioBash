e12455229b2973c264ac15f0c0d37c21510dc7b4
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2022-03-01 10:41:17 -0500

    add randomNumers.r

--- bin/randomNumbers.r
+++ bin/randomNumbers.r
@@ -1,7 +1,7 @@
 #!/usr/bin/env R
 numbers <- rep(-100, 15)
 numbers
-for (i in 1:15) { #0; i<numbers.length; i++) {
+for (i in 1:15) { 
     r = floor(runif(1, 1, 500))
     if(length(numbers[abs(numbers - r)>20])){
         numbers[i] = r
